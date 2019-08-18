6b25b989125b7c7f0e1567ed7d38b49007715214
masanori.kobori <livephotobank@yahoo.co.jp>
2019-08-18 11:14:51 +0900

    ページの推移した

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
